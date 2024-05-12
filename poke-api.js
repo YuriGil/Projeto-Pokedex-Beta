bc163c6298196980356cd7a84da3dc52092038d4
Yuri G <yurigink@gmail.com>
2024-05-11 21:38:08 -0300

    version test 1.0

--- poke-api.js
+++ poke-api.js
@@ -5,8 +5,8 @@ const pokeApi = {};
 //utilizando apenas as informações necessarias 
 function convertPokeApiDetailToPokemon(pokeDetail) {
   const pokemon = new Pokemon()
-  pokemon.number = pokeDetail.order
-  pokemon.name - pokeDetail.name
+  pokemon.number = pokeDetail.id
+  pokemon.name = pokeDetail.name
   
   const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
   const [type] = types
@@ -27,7 +27,7 @@ pokeApi.getPokemonDetail = (pokemon) => {
   .then(convertPokeApiDetailToPokemon)
 }
 
-pokeApi.getPokemons = (offset = 0, limit = 3) => {
+pokeApi.getPokemons = (offset = 0, limit = 5) => {
   const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
   /*codigo de consumo de api*/
 
